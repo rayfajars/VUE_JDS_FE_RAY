fd3ca8cf81ed2e21918496546d790d8837f3f1db
rayfajars <rayfajars@gmail.com>
2021-02-21 20:12:51 +0700

    f comit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <FormBantuan />
+  </div>
+</template>
+
+<script>
+import FormBantuan from './components/FormBantuan'
+
+export default {
+  name: 'App',
+  components: {
+    FormBantuan
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
